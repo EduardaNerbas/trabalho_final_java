08c6b85866de80ad30823578e1e9763f7bfc242a
Eduarda Gabriela Nerbas <eduarda.gabriela2104@gmail.com>
2025-11-03 20:56:18 -0300

    Começando finalmente

---
+++ src/test/java/com/TrabalhoFinalSpring/TrabalhoFinalSpringApplicationTests.java
@@ -0,0 +1,13 @@
+package com.TrabalhoFinalSpring;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TrabalhoFinalSpringApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
